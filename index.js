93843ea2500a21e9d31c83f2bdd66acb80b93e6e
NMoore42 <nicholas.moore5@icloud.com>
2019-05-20 13:37:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^-' A-Za-z0-9]+/g, '');
+  }
+
+  static titleize(string) {
+    let titleizedArr = [];
+    let noNoWords = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    let arr = string.split(" ")
+    let lowCaseArr = arr.map(word => word.toLowerCase())
+    lowCaseArr.map(word => {
+      if (!noNoWords.includes(word)) {
+        word = word[0].toUpperCase() + word.slice(1)
+        titleizedArr.push(word);
+      } else {
+        titleizedArr.push(word);
+      }
+    });
+    let joinedSentence =  titleizedArr.join(" ");
+    return joinedSentence[0].toUpperCase() + joinedSentence.slice(1)
+  }
 }
